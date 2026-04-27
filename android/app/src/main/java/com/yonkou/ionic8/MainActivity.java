ed64ad0ad04d5163c96fd21fbf044238aec4a6aa
Desmond8500 <desmond8500@gmail.com>
2026-04-27 06:45:44 +0000

    Suppression de l'importation et de l'enregistrement des plugins SQLite et IonicStorageModule dans MainActivity et main.ts

--- android/app/src/main/java/com/yonkou/ionic8/MainActivity.java
+++ android/app/src/main/java/com/yonkou/ionic8/MainActivity.java
@@ -1,13 +1,6 @@
 package com.yonkou.ionic8;
 
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.community.database.sqlite.CapacitorSQLite;
 
 public class MainActivity extends BridgeActivity {}
 
-
-@Override
-public void onCreate(Bundle savedInstanceState) {
-  super.onCreate(savedInstanceState);
-  registerPlugin(CapacitorSQLite.class);
-}
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
-import { IonicStorageModule } from '@ionic/storage-angular';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -15,7 +14,6 @@ bootstrapApplication(AppComponent, {
       mode: 'ios',
     }),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    importProvidersFrom(IonicStorageModule.forRoot()),
 
   ],
 });
